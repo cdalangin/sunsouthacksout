9a0919835d1a242ffa188eaf370fcb27807e8c49
cdalangin <florenzdalangin@gmail.com>
2020-08-29 18:14:13 -0400

    fixed index

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Sours from "./Sours"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Sours />,
+  <App />,
   document.getElementById('root')
 );
 
